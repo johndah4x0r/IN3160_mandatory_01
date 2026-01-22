98fc966ad0532f5ed161be0ad6537b072b38e91d
John Isaac Calderon <ogclapdrop@gmail.com>
2026-01-22 09:33:37 +0100

    Modified test bench + added .gitignore
    For some reason, the time in GTKWave is off by a factor of 1000.
    This has been compensated for in the test bench.
    
    Signed-off-by: John Isaac Calderon <ogclapdrop@gmail.com>

--- tb/tb_first.py
+++ tb/tb_first.py
@@ -5,44 +5,47 @@ import cocotb
 from cocotb.triggers import RisingEdge, Timer
 from cocotb.clock import Clock
 
+# Define primary time unit
+# - changed from 'ns' to 'us' to compensate
+#   for scale error in GTKWave
+TIME_UNIT = "us"
+
+
 @cocotb.test()
 async def main_test(dut):
-    
     """Try accessing the design."""
 
     dut._log.info("Running test...")
-    
+
     # Resetting unit
     dut.reset.value = 1
-    
+
     # Starting clock, at 50MHz
     dut._log.info("Starting clock")
-    cocotb.start_soon(Clock(dut.clk, 20, unit="ns").start())
-    
+    cocotb.start_soon(Clock(dut.clk, 20, unit=TIME_UNIT).start())
+
     # Running for 40ns
-    await Timer(40, unit="ns")
+    await Timer(40, unit=TIME_UNIT)
 
     # Setting inputs to zero, then wait for 10ns
     dut.inp.value = 0
     dut.load.value = 0
-    await Timer(10, unit="ns")
+    await Timer(10, unit=TIME_UNIT)
 
     # Clear reset, ...
     dut._log.info("Clearing reset")
     dut.reset.value = 0
-    await Timer(20, unit="ns")
-    
-    #Set input value
+    await Timer(20, unit=TIME_UNIT)
+
+    # Set input value
     dut.inp.value = 0b1010
     dut.load.value = 1
-    
-    #Wait for clock edge to rise
-    await RisingEdge(dut.clk)   
 
-    #Turn off load to let counting resume
+    # Wait for clock edge to rise
+    await RisingEdge(dut.clk)
+
+    # Turn off load to let counting resume
     dut.load.value = 0
 
-    await Timer(150, unit='ns')
+    await Timer(150, unit=TIME_UNIT)
     dut._log.info("Running test...done")
-
-    
